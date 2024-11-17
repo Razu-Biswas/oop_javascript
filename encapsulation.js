15accdd05b1c18657585a4aa43611129c64c3dc8
Razu-Biswas <sreekantaraju3421@gmail.com>
2024-11-17 12:29:54 +0600

    class and javascript oop example code

--- encapsulation.js
+++ encapsulation.js
@@ -1,23 +1,66 @@
-let name = "sakib al ah";
-let birthDay = "1987-03-24";
-let monthlySalary = 200000;
-let noOfMonths = 12;
+
+class Player {
+    constructor(name, birthDay, monthlySalary, noOfMonths) {
+        this.name = name;
+        this.birthDay = birthDay;
+        this.monthlySalary = monthlySalary;
+        this.noOfMonths = noOfMonths;
+
+    }
 
 
-function calculate_age(dob) {
-    const diff_ms = Date.now() - new Date(dob).getTime();
-    const age_dt = new Date(diff_ms);
+    calculate_age() {
+        const diff_ms = Date.now() - new Date(this.birthDay).getTime();
+        const age_dt = new Date(diff_ms);
 
-    return Math.abs(age_dt.getUTCFullYear() - 1970)
+        return Math.abs(age_dt.getUTCFullYear() - 1970)
+    }
+
+    getSalary() {
+        return (this.monthlySalary * this.noOfMonths).toLocaleString();
+    }
 }
 
 
-function getSalary(monthlySalary, noOfMonths) {
-    return (monthlySalary * noOfMonths).toLocaleString();
-}
+
+const Sakib = new Player("sakib", "1993-03-09", 200000, 12)
+const Skb = new Player("skb", "1993-03-09", 200000, 12)
+
+console.log(Sakib.calculate_age())
+
+console.log(Skb)
+
+// javascript oop example
+
+// let sakib = {
+
+//     name: "sakib al ah",
+//     birthDay: "1987-03-12",
+//     monthlySalary: 200000,
+//     noOfMonths: 12,
 
 
 
-console.log(calculate_age(noOfMonths));
+//     calculate_age() {
+//         const diff_ms = Date.now() - new Date(this.birthDay).getTime();
+//         const age_dt = new Date(diff_ms);
 
-console.log(getSalary(monthlySalary, noOfMonths))
+//         return Math.abs(age_dt.getUTCFullYear() - 1970)
+//     },
+
+//     getSalary() {
+//         return (this.monthlySalary * this.noOfMonths).toLocaleString();
+//     }
+
+
+// }
+
+
+
+
+
+
+
+// console.log(sakib.calculate_age());
+
+// console.log(sakib.getSalary())
