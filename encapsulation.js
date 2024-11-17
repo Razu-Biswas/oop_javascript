aada146537c34d229507604d79f1b651e0a90241
Razu-Biswas <sreekantaraju3421@gmail.com>
2024-11-17 12:52:26 +0600

    inheritance

--- encapsulation.js
+++ encapsulation.js
@@ -1,23 +1,27 @@
 
 class Player {
+    #name
+    #birthDay
+    #monthlySalary
+    #noOfMonths
     constructor(name, birthDay, monthlySalary, noOfMonths) {
-        this.name = name;
-        this.birthDay = birthDay;
-        this.monthlySalary = monthlySalary;
-        this.noOfMonths = noOfMonths;
+        this.#name = name;
+        this.#birthDay = birthDay;
+        this.#monthlySalary = monthlySalary;
+        this.#noOfMonths = noOfMonths;
 
     }
 
 
     calculate_age() {
-        const diff_ms = Date.now() - new Date(this.birthDay).getTime();
+        const diff_ms = Date.now() - new Date(this.#birthDay).getTime();
         const age_dt = new Date(diff_ms);
 
         return Math.abs(age_dt.getUTCFullYear() - 1970)
     }
 
     getSalary() {
-        return (this.monthlySalary * this.noOfMonths).toLocaleString();
+        return (this.#monthlySalary * this.#noOfMonths).toLocaleString();
     }
 }
 
--- inheritance.js
+++ inheritance.js
@@ -38,3 +38,62 @@ const monti = new Human("Monti")
 
 
 console.log(monti)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Base class (Parent)
+class Player {
+    constructor(name, level) {
+        this.name = name;
+        this.level = level;
+    }
+
+    play() {
+        return `${this.name} is playing at level ${this.level}.`;
+    }
+}
+
+// Derived class (Child)
+class FootballPlayer extends Player {
+    constructor(name, level, position) {
+        super(name, level); // Call the parent class constructor
+        this.position = position;
+    }
+
+    play() {
+        return `${this.name}, a level ${this.level} football player, plays as a ${this.position}.`;
+    }
+}
+
+// Another Derived class (Child)
+class BasketballPlayer extends Player {
+    constructor(name, level, team) {
+        super(name, level); // Call the parent class constructor
+        this.team = team;
+    }
+
+    play() {
+        return `${this.name}, a level ${this.level} basketball player, plays for the ${this.team} team.`;
+    }
+}
+
+// Creating instances of each class
+const footballPlayer = new FootballPlayer("Lionel", 99, "Forward");
+const basketballPlayer = new BasketballPlayer("LeBron", 97, "Lakers");
+
+// Using the play method
+console.log(footballPlayer.play()); // Output: Lionel, a level 99 football player, plays as a Forward.
+console.log(basketballPlayer.play()); // Output: LeBron, a level 97 basketball player, plays for the Lakers team.
