69f1a1fdc353efbce6eb78286c80fa8ab2f26273
Razu-Biswas <sreekantaraju3421@gmail.com>
2024-11-19 18:38:30 +0600

    poly example add

--- polymorphism.js
+++ polymorphism.js
@@ -1,7 +1,3 @@
-
-
-
-
 class Player {
     #name
     #birthDay
@@ -36,38 +32,3 @@ const Skb = new Player("skb", "1993-03-09", 200000, 12)
 console.log(Sakib.calculate_age())
 
 console.log(Skb)
-
-// javascript oop example
-
-// let sakib = {
-
-//     name: "sakib al ah",
-//     birthDay: "1987-03-12",
-//     monthlySalary: 200000,
-//     noOfMonths: 12,
-
-
-
-//     calculate_age() {
-//         const diff_ms = Date.now() - new Date(this.birthDay).getTime();
-//         const age_dt = new Date(diff_ms);
-
-//         return Math.abs(age_dt.getUTCFullYear() - 1970)
-//     },
-
-//     getSalary() {
-//         return (this.monthlySalary * this.noOfMonths).toLocaleString();
-//     }
-
-
-// }
-
-
-
-
-
-
-
-// console.log(sakib.calculate_age());
-
-// console.log(sakib.getSalary())
