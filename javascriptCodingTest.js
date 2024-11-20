ef71afa0d42a0d6f45fcc633aa32450cd7c4c62e
Razu-Biswas <sreekantaraju3421@gmail.com>
2024-11-21 00:28:16 +0600

    js constructor

--- javascriptCodingTest.js
+++ javascriptCodingTest.js
@@ -125,3 +125,29 @@ function Vehicle(model, color, year, country) {
     this.year = year;
     this.country = country;
 }
+
+
+// 6. class - multiple- constructor
+
+class Rectangle {
+    constructor(height, width) {
+        this.height = height;
+        this.width = width;
+    }
+
+    constructor(width) {
+        this.width = width;
+    }
+    // Getter
+    get area() {
+        return this.calcArea();
+    }
+    // Method
+    calcArea() {
+        return this.height * this.width;
+    }
+}
+
+const square = new Rectangle(20, 30);
+
+console.log(square.area); // Uncaught SyntaxError: A class may only have one constructor
